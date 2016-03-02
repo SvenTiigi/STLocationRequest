816a7675f1b1f369f796a4a5ed1ec06c019f735e
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-03-02 16:33:47 +0100

    Fixed code layout
    - Removed spaces and replaced with tabs
    - Fixed a few extra or missing spaces here and there

--- Pod/Source/UIViewController+STLocationRequest.swift
+++ Pod/Source/UIViewController+STLocationRequest.swift
@@ -10,26 +10,29 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    /*
-        UIViewController extension for showing the LocationRequestScreen
-    */
-    public func showLocationRequestController(setTitle title: String,setAllowButtonTitle allowButtonTitle : String,setNotNowButtonTitle notNowButtonTitle : String,setMapViewAlphaValue mapViewAlphaValue:CGFloat,setBackgroundViewColor backgroundViewColor: UIColor){
-        // Create the Bundle Path for Resources
-        let bundlePath = NSBundle(forClass: STLocationRequestController.self).pathForResource("STLocationRequest", ofType: "bundle")
-        let bundle = NSBundle(path: bundlePath!)
-        // Get the Storyboard File
-        let stb = UIStoryboard(name: "StoryboardLocationRequest", bundle:bundle)
-        // Instantiate the ViewController by Identifer as STLocationRequestController
-        let locationRequestViewController = stb.instantiateViewControllerWithIdentifier("locationRequestController") as! STLocationRequestController
-        // Passing the parameters Values
-        locationRequestViewController.titleLabelText = title
-        locationRequestViewController.allowButtonTitle = allowButtonTitle
-        locationRequestViewController.notNowButtonTitle = notNowButtonTitle
-        locationRequestViewController.mapViewAlphaValue = mapViewAlphaValue
-        locationRequestViewController.backgroundViewColor = backgroundViewColor
-        // Present the locationRequestViewController
-        self.presentViewController(locationRequestViewController, animated: true, completion: nil)
-    }
-    
-    
+	/*
+		UIViewController extension for showing the LocationRequestScreen
+	*/
+	public func showLocationRequestController(setTitle title: String, setAllowButtonTitle allowButtonTitle: String, setNotNowButtonTitle notNowButtonTitle: String, setMapViewAlphaValue mapViewAlphaValue: CGFloat, setBackgroundViewColor backgroundViewColor: UIColor) {
+		
+		// Create the Bundle Path for Resources
+		let bundlePath = NSBundle(forClass: STLocationRequestController.self).pathForResource("STLocationRequest", ofType: "bundle")
+		let bundle = NSBundle(path: bundlePath!)
+		
+		// Get the Storyboard File
+		let stb = UIStoryboard(name: "StoryboardLocationRequest", bundle:bundle)
+		
+		// Instantiate the ViewController by Identifer as STLocationRequestController
+		let locationRequestViewController = stb.instantiateViewControllerWithIdentifier("locationRequestController") as! STLocationRequestController
+		
+		// Passing the parameters Values
+		locationRequestViewController.titleLabelText = title
+		locationRequestViewController.allowButtonTitle = allowButtonTitle
+		locationRequestViewController.notNowButtonTitle = notNowButtonTitle
+		locationRequestViewController.mapViewAlphaValue = mapViewAlphaValue
+		locationRequestViewController.backgroundViewColor = backgroundViewColor
+		
+		// Present the locationRequestViewController
+		self.presentViewController(locationRequestViewController, animated: true, completion: nil)
+	}
 }
