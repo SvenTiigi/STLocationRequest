1415bae2802ef3893366b1fb34f7c9b71847bc6b
Sven Tiigi <sven.tiigi@gmail.com>
2018-01-29 12:16:04 +0100

    Updated gitignore

--- Example/Pods/Target Support Files/Font-Awesome-Swift/Font-Awesome-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Font_Awesome_Swift : NSObject
-@end
-@implementation PodsDummy_Font_Awesome_Swift
-@end
--- Example/Pods/Target Support Files/STLocationRequest/STLocationRequest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_STLocationRequest : NSObject
-@end
-@implementation PodsDummy_STLocationRequest
-@end
