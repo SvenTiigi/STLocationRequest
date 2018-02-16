eeca5c4a97da3298d2835602bdc36cebca6ebda0
Sven Tiigi <sven.tiigi@gmail.com>
2018-02-16 16:35:24 +0100

    Updated Test

--- STLocationRequestTests/STLocationRequestTests.swift
+++ STLocationRequestTests/STLocationRequestTests.swift
@@ -22,8 +22,7 @@ class STLocationRequestTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
     
 }
